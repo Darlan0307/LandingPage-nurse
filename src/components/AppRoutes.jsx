33cc8fe80658a80b89295b9e2df705a3b26c4370
Darlan0307 <darlanchagas2020@gmail.com>
2024-03-09 11:55:21 -0300

    :tada: Estrutural inicial do site

---
+++ src/components/AppRoutes.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AppRoutes = () => {
+  return (
+    <div>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, explicabo. Soluta repudiandae fugit quos esse suscipit nemo laborum impedit deserunt natus eligendi vero debitis illo maxime doloremque, consectetur, autem explicabo.</p>
+    </div>
+  )
+}
+
+export default AppRoutes
